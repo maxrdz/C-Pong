4d33505525c97a6f107af6e925c758532fd53c1f
Max Rodriguez <maxrcoppola@gmail.com>
2021-03-04 08:56:21 -0800

    Base Built

--- source/main.c
+++ source/main.c
@@ -6,8 +6,8 @@
   -------------------------------
 
     Author: 	Max Rodriguez
-    Date: 		3-3-2021
-    Revised: 	3-3-2021
+    Created: 	3-3-2021
+    Revised: 	3-4-2021
 
   -------------------------------
 
@@ -18,15 +18,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 /* Debugging Mode */
 #define DEBUG 1
 
+#if (DEBUG == 1)
+	const char* debug(char input[]) {
+		printf("%s", input);
+	}
+#else
+	const char* debug(char* input) {
+		// No Debug Output
+	}
+#endif
+
+void debugmode() {
+	char str[] = "----- Debugging Mode -----\n";
+	debug(str);
+}
+
 int main() {
 
-	#if (DEBUG == 1)
-		printf("DEBUG MODE");
-	#endif
+	debugmode();
 
 	return EXIT_SUCCESS;
 
